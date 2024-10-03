427a95a0264c8c5a4af9ad4b3aa4910322a396f1
nadiachaja <nadiagaja13@gmail.com>
2024-10-03 11:47:20 +0200

    home
    JavaScript toegepast
    wolkje verdwijnt als je op kruisje klikt
    
    tekst verandert na de tijd

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,24 @@
 
+/**wolkje verdwijnen als je op het kruisje likt */
+var button = document.querySelector(".kruisje")
+var cloud = document.querySelector(".wolkje")
+button.addEventListener("click", function(){
+   cloud.classList.add("hide")
+})
 
+/**text in wolkje dat de tijd veranderd na 12 uur en 18 uur */
+var today = new Date()
+var curHr = today.getHours() 
+var text = document.querySelector(".wolkje span")
+
+if (curHr <12){
+    text.textContent = "Good morning"
+}
+
+else if (curHr <18){
+    text.textContent = "Good afternoon"
+}
+
+else {
+   text.textContent = "Good evening"
+}
